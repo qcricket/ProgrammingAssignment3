12b50377d70f809ab7ab5e4b963d0581deb6c81c
Amy McPherson <qcricket@gmail.com>
2015-10-24 20:05:09 -0700

    Corrected syntax of lapply funciton lines 37, 44, 51

--- rankall.R
+++ rankall.R
@@ -5,12 +5,7 @@ rankall <- function(outcome, num = "best") {
   ## Read outcome data
   outcome.DF <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 
-  ## Check that state and outcome are valid
-  stateList <- unique(as.character(outcome.DF[,7]))
-  if(match(state, stateList, nomatch=-1) == -1){
-    stop ("invalid state")
-  }
-  
+  ## Check that outcome is valid
   validOutcome <- c("heart attack", "heart failure", "pneumonia")
   if(match(outcome, validOutcome, nomatch=-1) == -1){
     stop ("invalid outcome")
@@ -37,70 +32,40 @@ rankall <- function(outcome, num = "best") {
   
   ## Case 1 = Heart Attack
   if(outcome=="heart attack"){
-    df.HA <- df[df$HA != "Not Available", ]    
+    df <- df[df$HA != "Not Available", ]    
     ## Rank in ascending order by mortality rate
-    df.HA.Ordered <- lapply(x function(x) x[order(x$HA, x$Name), ])
-    
-    ## Generate output data frame    
-      if(num=="best"){
-        for(i in 1:54){
-        answer <- rbind(c(df.HA.Ordered[[i]]$Name[1], df.HA.Ordered[[i]]$State[1]))
-        }
+    df.Ordered <- lapply(x, function(x) x[order(x$HA, x$Name), ])
+  }
+  
+  ## Case 2 = Heart Failure
+  else if(outcome=="heart failure"){
+    df <- df[df$HF != "Not Available", ]    
+    ## Rank in ascending order by mortality rate
+    df.Ordered <- lapply(x, function(x) x[order(x$HF, x$Name), ])
+  }
+  
+  ## Case 3 = Pneumonia
+  else ## (outcome=="pneumonia"){
+    df <- df[df$PN != "Not Available", ]    
+    ## Rank in ascending order by mortality rate
+    df.Ordered <- lapply(x, function(x) x[order(x$PN, x$Name), ])
+      
+## Generate output data frame    
+  if(num=="best"){
+    for(i in 1:54){
+      answer <- rbind(c(df.Ordered[[i]]$Name[1], df.Ordered[[i]]$State[1]))
       }
+    }
       else if(num =="worst"){
         for(i in 1:54){
-          answer <- rbind(c(df.HA.Ordered[[i]]$Name[nrow(df.HA.Ordered[[i]])], df.HA.Ordered[[i]]$State[nrow(df.HA.Ordered[[i]])]))
+          answer <- rbind(c(df.Ordered[[i]]$Name[nrow(df.Ordered[[i]])], df.Ordered[[i]]$State[nrow(df.Ordered[[i]])]))
         }
       }
       else 
         for(i in 1:54){
-            answer <- rbind(c(df.HA.Ordered[[i]]$Name[nrow(df.HA.Ordered[[i]])], df.HA.Ordered[[i]]$State[nrow(df.HA.Ordered[[i]])]))
+            answer <- rbind(c(df.Ordered[[i]]$Name[nrow(df.Ordered[[i]])], df.Ordered[[i]]$State[nrow(df.Ordered[[i]])]))
         }
-    }
-  ## Case 2 = Heart Failure
-  else if(outcome=="heart failure"){
-    df.HF <- df[df$HF != "Not Available", ]    
-    ## Rank in ascending order by mortality rate
-    df.HF.Ordered <- lapply(x function(x) x[order(x$HF, x$Name), ])
-          
-    ## Generate output data frame
-      if(num=="best"){
-        for(i in 1:54){
-            answer <- rbind(c(df.HF.Ordered[[i]]$Name[1], df.HF.Ordered[[i]]$State[1]))
-        }
-        else if(num =="worst"){
-          for(i in 1:54){
-            answer <- rbind(c(df.HF.Ordered[[i]]$Name[nrow(df.HF.Ordered[[i]])], df.HF.Ordered[[i]]$State[nrow(df.HF.Ordered[[i]])]))
-            }
-        }
-        else 
-          for(i in 1:54){
-            answer <- rbind(c(df.HF.Ordered[[i]]$Name[nrow(df.HF.Ordered[[i]])], df.HF.Ordered[[i]]$State[nrow(df.HF.Ordered[[i]])]))
-            }
-        }
-  }
-  ## Case 3 = Pneumonia
-  else ## (outcome=="pneumonia"){
-    df.PN <- df[df$PN != "Not Available", ]    
-    ## Rank in ascending order by mortality rate
-    df.PN.Ordered <- lapply(x function(x) x[order(x$PN, x$Name), ])
-    
-    ## Generate output data frame    
-    if(num=="best"){
-      for(i in 1:54){
-        answer <- rbind(c(df.PN.Ordered[[i]]$Name[1], df.PN.Ordered[[i]]$State[1]))
-      }
-    }
-    else if(num =="worst"){
-      for(i in 1:54){
-        answer <- rbind(c(df.PN.Ordered[[i]]$Name[nrow(df.PN.Ordered[[i]])], df.PN.Ordered[[i]]$State[nrow(df.PN.Ordered[[i]])]))
-      }
-    }
-    else 
-      for(i in 1:54){
-        answer <- rbind(c(df.PN.Ordered[[i]]$Name[nrow(df.PN.Ordered[[i]])], df.PN.Ordered[[i]]$State[nrow(df.PN.Ordered[[i]])]))
-      }
-    
-  ## For each state, find the hospital of the given rank
-  answer
+
+## For each state, find the hospital of the given rank
+answer
 }
